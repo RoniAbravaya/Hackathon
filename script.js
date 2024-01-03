9261f7c3ba1949fc9e7467200b7fdc4d23631b0e
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2024-01-03 12:00:06 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+// onDragstart4pinkSquare(event): Initiates when the user starts dragging the pink box. It sets the data for the drag operation.
+
+// onDragend4pinkSquare(event): Executes when the user stops dragging the pink box. It's currently logging "end" to the console.
+
+// onDrag4PinkSquare(event): Updates the text content of the pink box with the current mouse coordinates during the drag.
+
+// onDragOver4orange(event): Prevents the default behavior during a drag over the orange box.
+
+// onDrop4orangeSquare(event): Handles the drop event when the pink box is dropped onto the orange box. It appends the pink box to the orange box, triggers a man animation, hides the orange box by reducing opacity, and reveals a hidden volunteer form.
+
+
+
 let isPinkBoxInOrange = false;
 
 function onDragstart4pinkSquare(event) {
