916f17a659bd979845885c31163148c1352d904e
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2024-01-03 11:45:04 +0200

    update

--- script.js
+++ script.js
@@ -1,59 +1,3 @@
-
-function onDragstart4pinkSquare(event) {
-    
-    
-    console.log("start");
-    event.dataTransfer.setData("text/plain", event.target.id);
-
-
-
-}
-
-
-function onDragend4pinkSquare(event) {
-
-    console.log("end");
-
-}
-
-
-function onDrag4PinkSquare(event) {
-
-    // console.log(event.clientX + " " + event.clientY);
-    event.target.textContent = event.clientX + " " + event.clientY;
-    globalId = event.target.id;
-
-
-}
-
-
-function onDragOver4orange(event) {
-    console.log("over orange");
-    event.preventDefault()
-
-
-
-
-}
-
-function onDrop4orangeSquare(event) {
-    console.log("Drop Orange");
-
-    const id = event.dataTransfer.getData("text/plain");
-    console.log(id);
-
-    const pinkSquare = document.getElementById(id);  
-    console.log(pinkSquare);
-
-    event.target.appendChild(pinkSquare);  
-}
-
-
-
-
-
-
-
 let isPinkBoxInOrange = false;
 
 function onDragstart4pinkSquare(event) {
@@ -92,5 +36,13 @@ function onDrop4orangeSquare(event) {
         const man = document.getElementById("man");
         man.style.animation = "dance 2s forwards"; // You can adjust the animation duration
         isPinkBoxInOrange = true;
+
+        // Hide the orange box by setting opacity to 0
+        const dropElement = document.getElementById("drop-element");
+        dropElement.style.opacity = 0;
+
+        // Show the user form by setting visibility to visible
+        const volunteerForm = document.getElementById("volunteerForm");
+        volunteerForm.style.visibility = "visible";
     }
 }
